01f79eb170b182d26544b23fe3d22ef8be9c2108
ArtGeas <Jellu9@mail.ru>
2023-06-26 13:06:51 +0300

    frontend

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -20,7 +20,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
 
 
 # Quick-start development settings - unsuitable for production
