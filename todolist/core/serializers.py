3eabe3b85d42aa869c508dc57e43e04dead66c35
ArtGeas <Jellu9@mail.ru>
2023-07-10 18:05:43 +0300

    create user

---
+++ todolist/core/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers, exceptions
+from django.contrib.auth.hashers import make_password
+
+from models import User
+from todolist.todolist.fields import PasswordField
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = PasswordField()
+    password_repeat = PasswordField()
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'password_repeat')
+
+    def validate(self, attrs: dict) -> dict:
+        if attrs['password'] != attrs['password_repeat']:
+            raise exceptions.ValidationError('Passwords must match')
+        return attrs
+
+    def create(self, validated_data: dict) -> User:
+        del validated_data['password_repeat']
+        validated_data['password'] = make_password(validated_data['password'])
+
+        super().create(validated_data)
---
+++ todolist/todolist/fields.py
@@ -0,0 +1,13 @@
+from typing import Any
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+
+class PasswordField(serializers.CharField):
+
+    def __init__(self, **kwargs: Any) -> None:
+        kwargs['style'] = {'input_type': 'password'}
+        kwargs.setdefault('write_only', True)
+        kwargs.setdefault('required', True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
