ec58c958a7d0d8ce9c8a733b56414b615f991b4b
ArtGeas <Jellu9@mail.ru>
2023-07-19 12:57:40 +0300

    goals functions

--- todolist/goals/admin.py
+++ todolist/goals/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory
+from goals.models import GoalCategory, Goal
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
@@ -8,4 +8,9 @@ class GoalCategoryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'user')
 
 
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'description', 'due_date', 'status', 'priority', 'created', 'updated')
+    search_fields = ('category', 'title', 'description')
+
 admin.site.register(GoalCategory, GoalCategoryAdmin)
+admin.site.register(Goal, GoalAdmin)
