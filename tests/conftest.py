97ca74680997e7099be861353cfb093775929b62
ArtGeas <Jellu9@mail.ru>
2023-08-11 10:53:23 +0300

    tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,23 @@
 # from pytest_factoryboy import register
 # from django.test import Client
 
-# from .factories import UserFactory
+from .factories import UserFactory
 
 # # Factories
-# register(UserFactory)
+register(UserFactory)
 # register(BoardFactory)
 # register(BoardParticipantFactory)
 # register(CategoryFactory)
 # register(GoalFactory)
 
 
-# @pytest.fixture
-# @pytest.mark.django_db
-# def authenticated_user():
-#     user = UserFactory.create()
-#     password = user.password
-#     user.set_password(password)
-#     user.save()
-#     client = Client()
-#     client.login(username=user.username, password=password)
-#     return {'client': client, 'user': user, 'password': password}
+@pytest.fixture
+@pytest.mark.django_db
+def authenticated_user():
+    user = UserFactory.create()
+    password = user.password
+    user.set_password(password)
+    user.save()
+    client = Client()
+    client.login(username=user.username, password=password)
+    return {'client': client, 'user': user, 'password': password}
